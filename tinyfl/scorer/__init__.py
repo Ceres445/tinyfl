ab4d8ec90cbe5442e2fec20218859eaf66bf63a6
Sarang S <email.sarangs@gmail.com>
2023-06-21 00:45:58 +0530

    ✨ roundless scorer node

--- tinyfl/scorer/__init__.py
+++ tinyfl/scorer/__init__.py
@@ -1,11 +1,15 @@
+import asyncio
 import json
 import logging
 from operator import itemgetter
 import os
 import sys
 import time
+from torch.utils.data import DataLoader
+
 
 from web3 import Web3
+from tinyfl.ipfs import load_model_ipfs
 
 from tinyfl.model import models, scorers
 
@@ -19,12 +23,13 @@ logger = logging.getLogger(__name__)
 with open(sys.argv[1]) as f:
     config = json.load(f)
     (
-        model,
+        cur_model,
         scorer,
         endpoint,
         registration_contract_address,
         round_free_contract_address,
         ipfs_host,
+        account,
     ) = itemgetter(
         "model",
         "scorer",
@@ -32,16 +37,22 @@ with open(sys.argv[1]) as f:
         "registration_contract_address",
         "round_free_contract_address",
         "ipfs_host",
+        "account",
     )(
         config
     )
-    model = models[model]
+    model = models[cur_model]
     scorer = scorers[scorer]
 
 logger.info(f"Model: {model.__name__}")
 logger.info(f"Scorer: {scorer.__name__}")
 
 w3 = Web3(Web3.HTTPProvider(endpoint))
+
+
+_, testset = model.create_datasets()
+testloader = DataLoader(testset, batch_size=64)
+model = models[cur_model].create_model()
 
 registration_contract = w3.eth.contract(
     address=Web3.to_checksum_address(registration_contract_address),
@@ -61,14 +72,27 @@ round_free_contract = w3.eth.contract(
 
 registration_contract.functions.registerDevice("scorer").transact()
 
+w3.eth.default_account = account
+
+
+async def score_model(cid: str):
+    print("it scorin time", cid)
+
+    model.load_state_dict(await load_model_ipfs(cid, ipfs_host))
+    accuracy, _ = model.test_model(testloader)
+    round_free_contract.functions.scoreModel(cid, accuracy).transact()
+
 
 def main():
     events = set()
+    last_seen_block = w3.eth.block_number
     while True:
         for event in round_free_contract.events.ModelScorers.create_filter(
-            fromBlock="latest"
+            fromBlock=last_seen_block
         ).get_new_entries():
             if event not in events:
                 events.add(event)
-                print(event)
+                last_seen_block = event["blockNumber"]
+                if w3.eth.default_account in event["args"]["scorers"]:
+                    asyncio.run(score_model(event["args"]["model"]))
         time.sleep(1)
