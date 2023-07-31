d962a172fa9e4821b855b206430149e772502c82
Druva D <ddruva445@gmail.com>
2023-07-31 15:29:29 +0530

    Normalized dataset

--- tinyfl/baseline/__init__.py
+++ tinyfl/baseline/__init__.py
@@ -20,7 +20,13 @@ from tinyfl.model import (
     splits,
     strategies,
 )
-from tinyfl.message import DeRegister, Register, StartSuperRound, SubmitSuperWeights, SubmitWeights
+from tinyfl.message import (
+    DeRegister,
+    Register,
+    StartSuperRound,
+    SubmitSuperWeights,
+    SubmitWeights,
+)
 
 batch_size = 64
 
@@ -112,7 +118,9 @@ async def handle(req: Request, background_tasks: BackgroundTasks):
             return {"success": True, "message": "Registered"}
         case SubmitSuperWeights(url=url, weights=weights, timestamp=timestamp):
             print("Received super weights")
-            background_tasks.add_task(collect_weights, url, copy.deepcopy(weights), timestamp)
+            background_tasks.add_task(
+                collect_weights, url, copy.deepcopy(weights), timestamp
+            )
             return {"success": True, "message": "Weights submitted"}
         case DeRegister(url=url, id=id):
             with client_lock:
@@ -122,13 +130,16 @@ async def handle(req: Request, background_tasks: BackgroundTasks):
         case _:
             return {"success": False, "message": "Unknown message"}
 
+
 @app.get("/logs")
-async def get_logs(): 
-    a = None 
+async def get_logs():
+    a = None
     b = None
     c = None
     d = None
-    if os.path.exists("super_aggregator_scores.csv") and os.path.exists("scores_aggregators.csv"):
+    if os.path.exists("super_aggregator_scores.csv") and os.path.exists(
+        "scores_aggregators.csv"
+    ):
         with open("super_aggregator_scores.csv", "r") as f:
             a = f.read()
         with open("scores_aggregators.csv", "r") as f:
@@ -138,7 +149,12 @@ async def get_logs():
             c = f.read()
         with open("perf_network.log", "r") as f:
             d = f.read()
-    return { "scores_aggregators": a, "super_aggregator_scores": b, "perf": c, "perf_network": d }
+    return {
+        "scores_aggregators": a,
+        "super_aggregator_scores": b,
+        "perf": c,
+        "perf_network": d,
+    }
 
 
 def state_manager():
@@ -158,10 +174,10 @@ def state_manager():
             return
         else:
             logger.info("Quorum achieved!")
-            #TODO: stop training after aggregation
+            # TODO: stop training after aggregation
             # asyncio.run(stop_training())
 
-            # Score each model 
+            # Score each model
             with open("scores_aggregators.csv", "a") as f:
                 writer = csv.writer(f)
                 for client, data in client_models.items():
@@ -232,23 +248,28 @@ async def start_training():
 
     # Tested to see if the indices are correct
     # print(client_len, list(map(lambda x: (x[0], len(x[1])), agg_indice)))
+    print("Checked length, sending start super round")
 
     async with httpx.AsyncClient() as client:
         # Switch from asyncio.gather to asyncio.wait
-        try: 
-            done, pending = await asyncio.wait( [
-                client.post(
-                    party,
-                    data=pickle.dumps(
-                        StartSuperRound(
-                            msg_id=next_msg_id(),
-                            weights=curr_weights,
-                            indices=indices,
-                        )
-                    ),
-                )
-                for party, indices in agg_indice
-            ], return_when=asyncio.ALL_COMPLETED, timeout=10)
+        try:
+            done, pending = await asyncio.wait(
+                [
+                    client.post(
+                        party,
+                        data=pickle.dumps(
+                            StartSuperRound(
+                                msg_id=next_msg_id(),
+                                weights=curr_weights,
+                                indices=indices,
+                            )
+                        ),
+                    )
+                    for party, indices in agg_indice
+                ],
+                return_when=asyncio.ALL_COMPLETED,
+                timeout=10,
+            )
             for i in done:
                 print(i.result())
             for i in pending:
--- tinyfl/models/fashion_mnist_model.py
+++ tinyfl/models/fashion_mnist_model.py
@@ -66,12 +66,28 @@ class FashionMNISTModel(nn.Module):
                 root="data",
                 train=True,
                 download=True,
-                transform=transforms.ToTensor(),
+                transform=transforms.Compose(
+                    [
+                        transforms.ToTensor(),
+                        transforms.Normalize(
+                            (0.5,),
+                            (0.5,),
+                        ),
+                    ]
+                ),
             ),
             datasets.FashionMNIST(
                 root="data",
                 train=False,
                 download=True,
-                transform=transforms.ToTensor(),
+                transform=transforms.Compose(
+                    [
+                        transforms.ToTensor(),
+                        transforms.Normalize(
+                            (0.5,),
+                            (0.5,),
+                        ),
+                    ]
+                ),
             ),
         )
